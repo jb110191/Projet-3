d076cb4fadc3737ac76c1fc4d08fc69bec0def4d
jb110191 <jb110191@hotmail.fr>
2018-04-23 10:57:44 +0200

    Création de la classe SelectionJeux pour choissir le jeux

--- com/main/Main.java
+++ com/main/Main.java
@@ -1,9 +1,8 @@
 package com.main;
 
 public class Main {
-
 	public static void main(String[] args) {
-		//
+		// On appelle la méthode ChoixJeux de la classe SelectionJeux pour faire le choix du jeux 
+		SelectionJeux.ChoixJeux();		
 	}
-
 }
