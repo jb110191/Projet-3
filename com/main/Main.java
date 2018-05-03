399df2e11383018b89361ff84a4e33f5d5b12aed
jb110191 <jb110191@hotmail.fr>
2018-05-03 11:22:50 +0200

    Ajout des fichiers

--- com/main/Main.java
+++ com/main/Main.java
@@ -1,8 +1,24 @@
 package com.main;
 
+import com.jeux.RecherchePlusMoins;
+
 public class Main {
 	public static void main(String[] args) {
 		// On appelle la méthode ChoixJeux de la classe SelectionJeux pour faire le choix du jeux 
-		SelectionJeux.ChoixJeux();		
+		//SelectionJeux.ChoixJeux();
+		
+		/*
+		 * Test
+		 */
+		RecherchePlusMoins xyz = new RecherchePlusMoins();
+		
+		xyz.Challenger();
+		
+		//xyz.Defenseur();
+		
+		// affiche quand le progamme se finit
+		System.out.println();
+		System.out.println();
+		System.out.println("fin du progamme");
 	}
 }
--- com/main/SelectionJeux.java
+++ com/main/SelectionJeux.java
@@ -1,7 +1,12 @@
 package com.main;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
-
+/**
+ * 
+ * @author Julian
+ *
+ */
 public class SelectionJeux {
 	/*
 	 * Variable de classe
@@ -26,35 +31,47 @@ public class SelectionJeux {
 		// Création d'un Scanner pour récupérer le choix puis sa fermeture
 		Scanner clavier = new Scanner(System.in);
 		
-		do
+		// Test
+		try
 		{
-			//Affichage du choix à chaque boucle
-			System.out.println("Choisis le jeux");
-			System.out.println("\t1 - Recherche +/-");
-			System.out.println("\t2 - Mastermind");
-			
-			// Récupération du choix
-			choix = clavier.nextInt();
-
-			switch(choix)
+			do
 			{
-			case 1 :
-				// On appelle ?
-				clavier.close();
-				break;
-			case 2 :
-				// On appelle ?
-				clavier.close();
-				break;
-			default :
-				System.out.println("Choisis une nombre valide, y a pas de jeux caché");
-				break;
-			}
+				//Affichage du choix à chaque boucle
+				System.out.println("Choisis le jeux");
+				System.out.println("\t1 - Recherche +/-");
+				System.out.println("\t2 - Mastermind");
+				
+				// Récupération du choix
+				choix = clavier.nextInt();
 
-		}while(choix > 1 && choix < 2);
+				switch(choix)
+				{
+				case 1 :
+					// On appelle ?
+					clavier.close();
+					break;
+				case 2 :
+					// On appelle ?
+					clavier.close();
+					break;
+				default :
+					System.out.println("Choisis une nombre valide, y a pas de jeux caché");
+					break;
+				}
+
+			}while(choix != 1 && choix != 2);
+			
+			// Affichage de la sortie du programme
+			System.out.println();
+			System.out.println("J'espère que le jeux t'as amusé.");
+		}
 		
-		// Affichage de la sortie du programme
-		System.out.println();
-		System.out.println("J'espère que le jeux t'as amusé.");
+		// Exeception
+		catch(InputMismatchException e)
+		{
+			System.out.println("catch");
+			clavier.close();
+			// Affichage de l'erreur
+		}
 	}
 }
